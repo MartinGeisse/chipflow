65aaac9947c535de301fa94ec7fc03e5750a0d32
Martin Geisse <martingeisse@googlemail.com>
2019-12-27 21:07:46 +0100

    initial checkin

---
+++ generated-scripts/qflow_vars.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/tcsh -f
+#-------------------------------------------
+# qflow variables for project /home/martin/git-repos/chipflow
+#-------------------------------------------
+
+set projectpath=/home/martin/git-repos/chipflow
+set techdir=/usr/share/qflow/tech/osu050
+set sourcedir=/home/martin/git-repos/chipflow
+set synthdir=/home/martin/git-repos/chipflow
+set layoutdir=/home/martin/git-repos/chipflow
+set techname=osu050
+set scriptdir=/usr/lib/qflow/scripts
+set bindir=/usr/lib/qflow/bin
+set synthlog=/home/martin/git-repos/chipflow/synth.log
+#-------------------------------------------
+
