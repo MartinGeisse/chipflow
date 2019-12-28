b138d9e1cdf49dec2b5ed1b5a351ddeefa8cc7fc
Martin Geisse <martingeisse@googlemail.com>
2019-12-28 15:04:02 +0100

    ...

--- resource/installed-scripts/placement.sh
+++ resource/installed-scripts/placement.sh
@@ -6,81 +6,22 @@
 # ".cel" and ".par" files.  It will run GrayWolf for the
 # placement.
 #----------------------------------------------------------
-# Tim Edwards, 5/16/11, for Open Circuit Design
-# Modified April 2013 for use with qflow
-# Modified November 2013 for congestion feedback
-#----------------------------------------------------------
 
-if ($#argv < 2) then
-   echo Usage:  placement.sh <project_path> <source_name>
-   exit 1
-endif
 
-# Split out options from the main arguments
-set argline=(`getopt "kdf" $argv[1-]`)
-set cmdargs=`echo "$argline" | awk 'BEGIN {FS = "-- "} END {print $2}'`
-set argc=`echo $cmdargs | wc -w`
 
-if ($argc == 2) then
-   set argv1=`echo $cmdargs | cut -d' ' -f1`
-   set argv2=`echo $cmdargs | cut -d' ' -f2`
-else
-   echo Usage:  placement.sh [options] <project_path> <source_name>
-   echo   where
-   echo       <project_path> is the name of the project directory containing
-   echo                 a file called qflow_vars.sh.
-   echo       <source_name> is the root name of the verilog file, and
-   echo       [options] are:
-   echo			-k	keep working files
-   echo			-d	generate DEF file for routing
-   echo			-f	final placement.  Don't run if routing succeeded
-   echo
-   echo	  Options to specific tools can be specified with the following
-   echo	  variables in project_vars.sh:
-   echo
-   echo
-   exit 1
-endif
-
-set keep=0
-set makedef=0
+# Usage:  placement.sh $projectpath $sourcename
+# option -k: set keep=1
+# option -d: set makedef=1
+# option -f: set final=1
+set keep = 0
+set makedef = 0
 set final = 0
-
-foreach option (${argline})
-   switch (${option})
-      case -k:
-         set keep=1
-         breaksw
-      case -d:
-         set makedef=1
-         breaksw
-      case -f:
-         set final=1
-	 breaksw
-      case --:
-         break
-   endsw
-end
-
-set projectpath=$argv1
-set sourcename=$argv2
 set rootname=${sourcename:h}
-
-# This script is called with the first argument <project_path>, which should
-# have file "qflow_vars.sh".  Get all of our standard variable definitions
-# from the qflow_vars.sh file.
-
-if (! -f ${projectpath}/qflow_vars.sh ) then
-   echo "Error:  Cannot find file qflow_vars.sh in path ${projectpath}"
-   exit 1
-endif
 
 source ${projectpath}/qflow_vars.sh
 source ${techdir}/${techname}.sh
 cd ${projectpath}
-if (-f project_vars.sh) then
-   source project_vars.sh
-endif
+source project_vars.sh
 
 # Prepend techdir to leffile unless leffile begins with "/"
 set abspath=`echo ${leffile} | cut -c1`
@@ -529,12 +470,3 @@ if ($keep == 0) then
    rm -f ${rootname}.mver ${rootname}.mvio ${rootname}.stat ${rootname}.out
    rm -f ${rootname}.pth ${rootname}.sav ${rootname}.scel ${rootname}.txt
 endif
-
-#------------------------------------------------------------
-# Done!
-#------------------------------------------------------------
-
-set endtime = `date`
-echo "Placement script ended on $endtime" >> $synthlog
-
-exit 0
